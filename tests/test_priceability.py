bc8f8c2b1c51d5ac6afc09d42d513944babe3bd9
drknzz <kamil.jankowski.x@gmail.com>
2024-04-06 18:53:21 +0200

    Add test_priceability.py

---
+++ tests/test_priceability.py
@@ -0,0 +1,138 @@
+"""
+Module testing priceability.
+"""
+from unittest import TestCase
+
+from pabutools.analysis.priceability import priceable, validate_price_system
+from pabutools.election import Project, Instance, ApprovalProfile, ApprovalBallot
+
+
+class TestPriceability(TestCase):
+    def test_priceable_approval(self):
+        # Example from https://arxiv.org/pdf/1911.11747.pdf page 2
+
+        # +----+----+----+
+        # | c4 | c5 | c6 |
+        # +----+----+----+----+-----+-----+
+        # |      c3      | c9 | c12 | c15 |
+        # +--------------+----+-----+-----+
+        # |      c2      | c8 | c11 | c14 |
+        # +--------------+----+-----+-----+
+        # |      c1      | c7 | c10 | c13 |
+        # +===============================+
+        # | v1 | v2 | v3 | v4 | v5  | v6  |
+
+        p = [Project(str(i), cost=1) for i in range(16)]
+        instance = Instance(p[1:], budget_limit=12)
+
+        v1 = ApprovalBallot({p[1], p[2], p[3], p[4]})
+        v2 = ApprovalBallot({p[1], p[2], p[3], p[5]})
+        v3 = ApprovalBallot({p[1], p[2], p[3], p[6]})
+        v4 = ApprovalBallot({p[7], p[8], p[9]})
+        v5 = ApprovalBallot({p[10], p[11], p[12]})
+        v6 = ApprovalBallot({p[13], p[14], p[15]})
+        profile = ApprovalProfile(init=[v1, v2, v3, v4, v5, v6])
+
+        allocation = p[1:4] + p[7:]
+        self.assertIsNone(priceable(instance, profile, allocation))
+
+        allocation = p[1:9] + p[10:12] + p[13:15]
+        self.assertIsNotNone(priceable(instance, profile, allocation))
+
+        allocation, b, pf = priceable(instance, profile, extra_output=True)
+        self.assertIsNotNone(priceable(instance, profile, allocation, b, pf))
+        self.assertIsNotNone(priceable(instance, profile, allocation))
+
+        self.assertTrue(validate_price_system(instance, profile, allocation, b, pf))
+
+    def test_is_priceable_approval_extended(self):
+        # Example from https://arxiv.org/pdf/1911.11747.pdf page 15 (k = 5)
+
+        # +------------------------+
+        # |           c10          |
+        # +------------------------+
+        # |           c9           |
+        # +------------------------+
+        # |           c8           |
+        # +------------------------+
+        # |           c7           |
+        # +------------------------+
+        # |           c6           |
+        # +----+----+----+----+----+
+        # | c1 | c2 | c3 | c4 | c5 |
+        # +========================+
+        # | v1 | v2 | v3 | v4 | v5 |
+
+        p = [Project(str(i), cost=1) for i in range(11)]
+        instance = Instance(p[1:], budget_limit=5)
+
+        v1 = ApprovalBallot({p[1], p[6], p[7], p[8], p[9], p[10]})
+        v2 = ApprovalBallot({p[2], p[6], p[7], p[8], p[9], p[10]})
+        v3 = ApprovalBallot({p[3], p[6], p[7], p[8], p[9], p[10]})
+        v4 = ApprovalBallot({p[4], p[6], p[7], p[8], p[9], p[10]})
+        v5 = ApprovalBallot({p[5], p[6], p[7], p[8], p[9], p[10]})
+        profile = ApprovalProfile(init=[v1, v2, v3, v4, v5])
+
+        allocation = p[1:3]
+        self.assertIsNone(priceable(instance, profile, allocation))
+
+        allocation = p[1:6]
+        self.assertIsNotNone(priceable(instance, profile, allocation))
+
+        allocation = p[6:]
+        self.assertIsNotNone(priceable(instance, profile, allocation))
+
+        allocation, b, pf = priceable(instance, profile, extra_output=True)
+        self.assertIsNotNone(priceable(instance, profile, allocation, b, pf))
+        self.assertIsNotNone(priceable(instance, profile, allocation))
+
+        self.assertTrue(validate_price_system(instance, profile, allocation, b, pf))
+
+    def test_is_priceable_approval_extended2(self):
+        # Example from http://www.cs.utoronto.ca/~nisarg/papers/priceability.pdf page 13
+
+        # +--------------+--------------+--------------+
+        # |      c6      |      c9      |      c12     |
+        # +--------------+--------------+--------------+
+        # |      c5      |      c8      |      c11     |
+        # +--------------+--------------+--------------+
+        # |      c4      |      c7      |      c10     |
+        # +--------------+--------------+--------------+
+        # |                     c3                     |
+        # +--------------------------------------------+
+        # |                     c2                     |
+        # +--------------------------------------------+
+        # |                     c1                     |
+        # +============================================+
+        # | v1 | v2 | v3 | v4 | v5 | v6 | v7 | v8 | v9 |
+
+        p = [Project(str(i), cost=1) for i in range(13)]
+        instance = Instance(p[1:], budget_limit=9)
+
+        v1 = ApprovalBallot({p[1], p[2], p[3], p[4], p[5], p[6]})
+        v2 = ApprovalBallot({p[1], p[2], p[3], p[4], p[5], p[6]})
+        v3 = ApprovalBallot({p[1], p[2], p[3], p[4], p[5], p[6]})
+
+        v4 = ApprovalBallot({p[1], p[2], p[3], p[7], p[8], p[9]})
+        v5 = ApprovalBallot({p[1], p[2], p[3], p[7], p[8], p[9]})
+        v6 = ApprovalBallot({p[1], p[2], p[3], p[7], p[8], p[9]})
+
+        v7 = ApprovalBallot({p[1], p[2], p[3], p[10], p[11], p[12]})
+        v8 = ApprovalBallot({p[1], p[2], p[3], p[10], p[11], p[12]})
+        v9 = ApprovalBallot({p[1], p[2], p[3], p[10], p[11], p[12]})
+        profile = ApprovalProfile(init=[v1, v2, v3, v4, v5, v6, v7, v8, v9])
+
+        allocation = p[1:10]
+        self.assertIsNotNone(priceable(instance, profile, allocation))
+
+        allocation = p[1:6] + p[7:9] + p[10:12]
+        self.assertIsNotNone(priceable(instance, profile, allocation))
+
+        allocation = p[1:6] + p[7:9] + p[11:12]
+        self.assertIsNone(priceable(instance, profile, allocation))
+
+        allocation, b, pf = priceable(instance, profile, extra_output=True)
+        self.assertIsNotNone(priceable(instance, profile, allocation, b, pf))
+        self.assertIsNotNone(priceable(instance, profile, allocation))
+
+        self.assertTrue(validate_price_system(instance, profile, allocation, b, pf))
